766eca4a66bf8fa8cd1dc087c543c41f03152c2a
Luis Gustavo <luisgustavo.email10@gmail.com>
2026-05-05 17:58:38 -0300

    Computed Signals

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -15,5 +15,21 @@ export class Hello {
   protected onClick() :void{
     console.log("Button clicked");
     this.isDisable = !this.isDisable;
+  }
+
+  protected count = signal(0);
+
+  protected doubleCount = computed( ()=> this.count() * 2); // a função comuted calcula um valor a partir de um signal
+
+  protected increaseCounter() :void{
+    this.count.update(value => value + 1);
+  }
+
+  protected decreaseCounter() :void{
+    this.count.update(value => value - 1);
+  }
+
+  protected resetCounter() :void{
+    this.count.set(0);
   }
 }
